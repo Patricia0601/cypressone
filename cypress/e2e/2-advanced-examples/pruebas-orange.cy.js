fb11420551961b6024e208e4c2aa37947c657ebb
Patricia0601 <129229351+Patricia0601@users.noreply.github.com>
2023-03-31 02:39:13 +0200

    debug

--- cypress/e2e/2-advanced-examples/pruebas-orange.cy.js
+++ cypress/e2e/2-advanced-examples/pruebas-orange.cy.js
@@ -28,7 +28,7 @@ describe ('Pruebas de validacion funcion adicionar usuario', function (){
    
   it.only('Prueba E2E para adicionar un usuario', function (){
     cy.visit("https://opensource-demo.orangehrmlive.com/")
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').debug().type('Admin')
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
     cy.get('.oxd-button').click()
     cy.get(':nth-child(1) > .oxd-main-menu-item').click()
@@ -37,7 +37,7 @@ describe ('Pruebas de validacion funcion adicionar usuario', function (){
     cy.get('.oxd-autocomplete-dropdown > :nth-child(3)').click()
     //generacion de un numero aleatorio para crear siempre un usuario distinto y omitir validacion de duplicados
     let rndnum = Math.random() *2;
-    cy.get(':nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Rene1234")
+    cy.get(':nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input').type("jose234")
     cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').type("Alex--1234")
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Alex--1234")
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon').click()
